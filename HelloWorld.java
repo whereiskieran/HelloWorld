7463c134ca688a77c733fb52389bfa2e4be2385a
Kieran <whereiskieran@gmail.com>
2019-01-17 08:11:43 +0000

    Added HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+
+      // Prints "Hello, World" in the terminal window.
+		for (int i = 0; i < 10; i++){
+        	System.out.println("Hello, World"+i);
+		}
+   	}
+}
