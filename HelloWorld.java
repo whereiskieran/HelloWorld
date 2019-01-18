5f8af26a35f2ed3335af0357efccec4a2bbbc935
Kieran O'Callaghan <whereiskieran@gmail.com>
2019-01-18 08:04:29 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
 
       // Prints "Hello, World" in the terminal window.
-		for (int i = 0; i < 10; i++){
+		for (int i = 0; i < 22; i++){
         	System.out.println("Hello, World"+i);
 		}
    	}
