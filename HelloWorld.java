34057f103f0b93c6dbd996fdf965f6175f3ed044
Kieran O'Callaghan <whereiskieran@gmail.com>
2019-01-21 09:00:08 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
 
       // Prints "Hello, World" in the terminal window.
+	   System.out.println("Code downloaded from GIT hub");"
 		for (int i = 0; i < 22; i++){
 		System.out.println("Monday mourning");
         	System.out.println("Hello, World in GIT "+i);
